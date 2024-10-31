b31d414fb61e20bf4b00efd9e872cb2e5b4b4305
Basile Pesin <basile.pesin@vertmo.org>
2024-10-31 17:01:30 +0100

    Added some simulation primitives for numworks

--- targets/numworks/byterun/simul/sf-regs.c
+++ targets/numworks/byterun/simul/sf-regs.c
@@ -1,6 +1,48 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
 /******************************************************************************/
 /******************************************************************************/
 /******************************************************************************/
+
+void delay_usec(uint64_t us) {
+  micro_sleep(us);
+}
+
+/******************************************************************************/
+
+void display_draw_string(const char *text, uint16_t x, uint16_t y) {
+  printf("%s", text);
+}
+
+void display_draw_string_small(const char *text, uint16_t x, uint16_t y) {
+  printf("%s", text);
+}
+
+void display_draw_string_full(const char *text, uint16_t x, uint16_t y, bool large_font, uint16_t text_color, uint16_t bg_color) {
+  printf("%s", text);
+}
+
+void display_push_rect_uniform(uint16_t color, uint16_t x, uint16_t y, uint16_t width, uint16_t height) {
+  // TODO
+}
+
+void display_push_allscreen_uniform(uint16_t color) {
+  // TODO
+}
+
+/******************************************************************************/
+
+uint8_t brightness = 0;
+
+uint8_t eadk_backlight_brightness() {
+  return brightness;
+}
+
+void eadk_backlight_set_brightness(uint8_t bright) {
+  brightness = bright;
+}
 
 /******************************************************************************/
 /******************************************************************************/
