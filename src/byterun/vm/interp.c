63922934e6cb4a5ba8c8955f3f346466e1e1c426
Basile Pesin <basile.pesin@vertmo.org>
2024-11-14 11:55:41 +0100

    Fixed bug in CLOSUREREC_2B and CLOSUREREC_4B

--- src/byterun/vm/interp.c
+++ src/byterun/vm/interp.c
@@ -793,7 +793,7 @@ static inline void interp(void) {
       }
       push(acc);
       for (i = 1; i < f; i ++){
-        push(Ram_field(acc, 2 * i));
+        push(acc + Offsetclosure_delta * i);
       }
        break;
     }
@@ -820,7 +820,7 @@ static inline void interp(void) {
       }
       push(acc);
       for (i = 1; i < f; i ++){
-        push(Ram_field(acc, 2 * i));
+        push(acc + Offsetclosure_delta * i));
       }
       break;
     }
