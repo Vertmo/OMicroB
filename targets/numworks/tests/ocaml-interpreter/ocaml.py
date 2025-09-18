51e39d0314bbdb7ee92721a625cfbb277dae3141
Basile Pesin <basile.pesin@vertmo.org>
2025-09-18 17:31:04 +0200

    Removec ocaml-interpreter example

--- targets/numworks/tests/ocaml-interpreter/ocaml.py
+++
@@ -1,9 +0,0 @@
-(* Example of an OCaml script to be used by the OCaml-Numworks app *)
-let rec fibonacci n =
-  if n <= 1 then
-    n
-  else
-    fibonacci (n-1) + fibonacci (n-2)
-in
-(* - : string * (int -> int) * int = ("A Fibonaci function", <fun>, 610) *)
-"A Fibonacci function", fibonacci, fibonacci 15;;
